871a68f7e764ccb3435acda266f3954c358d99f9
Tyrik Gibbs <tyrikgibbs@outlook.com>
2019-12-10 21:03:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,4 +8,4 @@ document.body.appendChild(element)
 let newHeader = document.getElementById('victory');
 
 content = someTextArea.value;
-newHeader.innerHTML = ('${content} is the champion');
+elements.innerHTML = ('${content} is the champion');
