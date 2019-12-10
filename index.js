dd3bb4928d10d46f376945f3a26a573ef6b3fa00
Tyrik Gibbs <tyrikgibbs@outlook.com>
2019-12-10 20:54:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,4 +6,4 @@ let element = document.createElement('h1');
 element.id = ('victory');
 document.body.appendChild(element)
 let newHeader = document.getElementById('h1#victory');
-header.innerHTML =
+
