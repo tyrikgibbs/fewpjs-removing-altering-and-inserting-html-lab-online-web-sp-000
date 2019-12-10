0cc3199b37042a6fa2c3cc32c331627a85caac89
Tyrik Gibbs <tyrikgibbs@outlook.com>
2019-12-10 22:55:46 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+main.remove()
+
+
+let element = document.createElement('h1');
+element.id = ('victory');
+document.body.appendChild(element)
+
+let newHeader = document.getElementById('victory');
+
+
+newHeader.innerHTML = ('Tyrik is the champion');
