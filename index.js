4fc6f6d90bc8f3d5dd04a7c21fa1e8f14a0d86b5
Tyrik Gibbs <tyrikgibbs@outlook.com>
2019-12-10 20:54:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,4 +5,5 @@ main.remove()
 let element = document.createElement('h1');
 element.id = ('victory');
 document.body.appendChild(element)
-let newHeader = document.getElementById('victory');
+let newHeader = document.getElementById('h1#victory');
+header.innerHTML =
