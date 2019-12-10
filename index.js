473c0e25ffed5b12a46a83371458719ce38eae8a
Tyrik Gibbs <tyrikgibbs@outlook.com>
2019-12-10 20:57:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,4 +6,6 @@ let element = document.createElement('h1');
 element.id = ('victory');
 document.body.appendChild(element)
 let newHeader = document.getElementById('victory');
-header.innerHTML =
+
+content = someTextArea.value
+newHeader.innerHTML = '{#content} is the champion'
