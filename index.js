aa3c3338da9463736c12c5a6a01d9f4cf167b634
Tyrik Gibbs <tyrikgibbs@outlook.com>
2019-12-10 20:12:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Write your code here!
 main.remove()
 
-let element = document.createElement("h1#victory");
-var newHeader = document.querySelector("h1#victory");
+let element = document.createElement("h1");
+let newHeader = document.querySelector("h1#victory");
